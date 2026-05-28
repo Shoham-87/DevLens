13477d89cfc703a0915362a650ca8934037c9ca4
Shoham Dey Sarkar <shohamdeysarkar@gmail.com>
2026-05-28 12:36:14 +0530

    Initial Commit

---
+++ devlens/src/main/java/com/sds/devlens/DevlensApplication.java
@@ -0,0 +1,13 @@
+package com.sds.devlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlensApplication.class, args);
+	}
+
+}
---
+++ devlens/src/test/java/com/sds/devlens/DevlensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.devlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
