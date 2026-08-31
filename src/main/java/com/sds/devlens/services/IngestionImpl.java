363894bafa33525e8c264c6196cf5e194af84cc8
Shoham Dey Sarkar <65813417+Shoham-87@users.noreply.github.com>
2026-08-31 23:53:04 +0530

    Adding Logout button in UI

--- src/main/java/com/sds/devlens/services/IngestionImpl.java
+++ src/main/java/com/sds/devlens/services/IngestionImpl.java
@@ -1,11 +1,9 @@
 package com.sds.devlens.services;
 
 import com.sds.devlens.enums.IngestionEnum;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class IngestionImpl implements Ingestion{
